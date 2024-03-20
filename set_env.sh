9456424e35a38f39c656a4dcdbd8d42f406cdcca
vraghavi <vannalaraghavi@gmail.com>
2024-03-20 13:44:06 -0500

    monolith to microservice

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=clouddevex3
+export POSTGRES_PASSWORD=udagram12
+export POSTGRES_HOST=udagram-rv.cxbakcxns0yl.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::udagram-rv
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=ex3
 export JWT_SECRET=testing
 export URL=http://localhost:8100
